f384ea6bcdffd578288533308c16b8d4a0351ef5
YPerezM <yperezmartinez@danielcastelao.org>
2016-02-24 10:34:19 +0100

    Commit inicial

---
+++ theme_YPerezM/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'TemaYago',
+    'description': 'Default website theme to showcase customization possibilities.',
+    'category': 'Theme/Hidden',
+    'sequence': 1000,
+    'version': '1.0',
+    'author': 'YPerezM',
+    'depends': ['website'],
+    'data': [
+        'views/templates.xml',
+    ],
+    'images': [
+        'static/description/cover.png',
+    ],
+    'application': False,
+}
