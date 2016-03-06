4cfe8a822d405765b736a2981ba820b55cb57e60
yago <yago@192.168.1.149>
2016-03-06 20:44:48 +0100

    Layout Bienvenido a mi pagina hecho

---
+++ theme_Yago/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Tema_Yago',
+    'description': 'Repasando en casa el funcionamiento de Odoo',
+    'category': 'Theme/Hidden',
+    'sequence': 1000,
+    'version': '1.0',
+    'author': 'Yago Perez Martinez',
+    'depends': ['website'],
+    'data': [
+        'views/templates.xml','views/layout.xml',
+    ],
+    'images': [
+        'static/description/icon.png',
+    ],
+    'application': False,
+}
